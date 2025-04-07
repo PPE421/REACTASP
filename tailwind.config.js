724b320630902af1eaae7bd83895fad301a8093a
Andres Riaño <pipe421ri@gmail.com>
2025-04-06 20:34:09 -0500

    Agregando styles con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class', // habilita modo oscuro con clases
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
